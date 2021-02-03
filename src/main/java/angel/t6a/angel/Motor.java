1c432f5cc850f35ae4c5eb753ed65dc8b3f3f1b6
Angel <angelbmx002@gmail.com>
2021-02-03 14:07:16 +0100

    Crea las clases

---
+++ src/main/java/angel/t6a/angel/Motor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package angel.t6a.angel;
+
+/**
+ *
+ * @author Ángel
+ */
+public class Motor {
+    private int cilindros;
+    private int combus;
+}
---
+++ src/main/java/angel/t6a/angel/MotorCoche.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package angel.t6a.angel;
+
+/**
+ *
+ * @author Ángel
+ */
+public class MotorCoche extends Motor{
+    
+}
