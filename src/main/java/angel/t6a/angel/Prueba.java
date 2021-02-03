77f0e81c439955527d70c4af040ae9630a04bf4b
Angel <angelbmx002@gmail.com>
2021-02-03 17:58:23 +0100

    Crea la lista de objetos de la superclase A y añade dos objetos de cada

--- src/main/java/angel/t6a/angel/Prueba.java
+++ src/main/java/angel/t6a/angel/Prueba.java
@@ -5,6 +5,8 @@
  */
 package angel.t6a.angel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Ángel
@@ -13,5 +15,26 @@ public class Prueba {
 
     public static void main(String[] args) {
 
+        // Creamos la lista de objetos de la superclase A y vamos a añadir dos 
+        // objetos de cada
+        ArrayList<Motor> almacenMotores = new ArrayList<>();
+        
+        // Motor
+        almacenMotores.add (new Motor(4, "Gasolina"));
+        almacenMotores.add (new Motor(3, "Diésel"));
+        
+        // MotorCoche
+        almacenMotores.add(new MotorCoche(1400));
+        almacenMotores.add(new MotorCoche(1900));
+        
+        // Berlina
+        almacenMotores.add(new Berlina("BMW", 3));
+        almacenMotores.add(new Berlina("Audi", 5));
+        
+        // Furgoneta
+        almacenMotores.add(new Furgoneta("Volkswagen", "Caravelle"));
+        almacenMotores.add(new Furgoneta("Citroën", "Jumpy"));
+        
+        
     }
 }
